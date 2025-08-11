f62fffb062c8763143773e7d8b39bc5063e8d2ff
alexwhiting <131557575+alexwhiting@users.noreply.github.com>
2025-08-11 20:04:11 -0300

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+
+[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
@@ -23,6 +25,12 @@ export function addToCart(productId) {
     quantity: 1
     });
   }
+
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function removeFromCart(productId) {
@@ -35,4 +43,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
